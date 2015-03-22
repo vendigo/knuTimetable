88f0d62d5ca9a6d5abc7ff31bf20d92d34a9ff6a
ace-vendigo <ace.vendigo@gmail.com>
2015-03-22 14:28:49 +0200

    faculty page, list of groups, timeTable added

--- src/main/webapp/js/facultiesController.js
+++
@@ -1,5 +0,0 @@
-app.controller('facultiesController', function($scope, $http) {
-	$http.get('/guest/faculty').success(function(response) {
-		$scope.faculties = response;
-	});
-});
--- src/main/webapp/js/pageController.js
+++ src/main/webapp/js/pageController.js
@@ -1,4 +1,4 @@
-app.controller('pageController', function($scope) {
+app.controller('pageController', function($scope, $http) {
 	/*Init*/
 	$scope.activePage = 'main';
 
@@ -12,7 +12,47 @@ app.controller('pageController', function($scope) {
 	};
 	/*Changes activePage to given*/
 	$scope.goTo = function(page) {
-		$scope.activePage = page;
-	}
+		$scope.activePage = page;		
+	};
+
+	/*FACULTIES PART*/
+	/*Get Faculties from server*/	
+	$scope.loadFaculties = function() {
+		$http.get('/guest/faculty').success(function(response) {
+		$scope.faculties = response;
+		});
+	};
+	/*Returns true if faculties are loaded and there is no active faculty*/
+	$scope.isListFaculties = function() {
+		return $scope.faculties && $scope.activeFaculty === '';
+	};
+	/*Returns true if faculties are loaded and there is active faculty*/
+	$scope.isFaculty = function() {
+		return $scope.faculties && $scope.activeFaculty != '';
+	};
+	/*Changes activeFaculty to given*/
+	$scope.goToFaculty = function(faculty) {
+		return $scope.activeFaculty = faculty;
+	};
+	/*Goes to faculties page*/
+	$scope.goToFaculties = function() {
+			$scope.goTo('faculties');
+			$scope.loadFaculties();
+			$scope.goToFaculty('');
+		};
+
+	/*TIMETABLE PART	*/
+	/*Loads actualTimeTable for given group*/
+	$scope.loadTimeTable = function(group) {
+		var request = '/guest/group/'+group.id+'/actualTimeTable';
+		$http.get(request).success(function(response) {
+		$scope.timeTable = response;
+		});
+	};
+	$scope.goToTimeTable = function(group) {
+		$scope.goTo('timeTable');
+		$scope.loadTimeTable(group);		
+		};
+	
 });
 
